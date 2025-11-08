f440fa87373cb1e38917d829978b7dc1b5019b71
Ujjawal Saini <ujjawalsaini@2004@gmail.co>
2025-11-08 09:18:20 +0530

    loops

---
+++ loops/sumofdigits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int ld ;
+    int sum = 0;
+    while(n!=0){
+        ld = n%10;
+        sum = sum + ld;
+        n = n/10;
+    }
+    cout<<"Total sum of digits are : "<<sum;
+    }
