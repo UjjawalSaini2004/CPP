b5fb9469c987e7bde45f4b07771427c2bd58e2a3
Ujjawal Saini <ujjawalsaini010@gmail.com>
2025-11-12 22:49:53 +0530

    pattern

---
+++ Pattern/starrectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){ 
+    int n,m;
+    cout<<"Enter number of rows"<<endl;
+    cin>>n;
+     cout<<"Enter number of columnw"<<endl;
+     cin>>m;
+     for(int i = 1;i<=n;i++){   // rows
+     for(int i = 1;i<=m;i++){   // columns
+        cout<<"*";
+       }
+       cout<<endl;
+    }
+    }
+    
