33e1a784268801d57307c05b8c4e78411209ce6c
Ujjawal Saini <ujjawalsaini010@gmail.com>
2025-11-12 23:23:08 +0530

    pattern

--- Pattern/starsquare.cpp
+++ Pattern/starsquare.cpp
@@ -1,16 +1,14 @@
 #include<iostream>
 using namespace std;
 int main (){ 
-    int n,m;
+    int n;
     cout<<"Enter number of rows"<<endl;
     cin>>n;
-     cout<<"Enter number of columnw"<<endl;
-     cin>>m;
      for(int i = 1;i<=n;i++){   // rows
-     for(int j = 1;j<=m;j++){   // columns
+     for(int j = 1;j<=n;j++){   // columns
         cout<<"*";
        }
        cout<<endl;
     }
     }
-    
+     
